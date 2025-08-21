6fd35b342655ae49d3b830e2dd737f4e522f1b76
Cardoso8586 <jucelino.7785mendes@gmail.com>
2025-08-21 11:10:05 -0300

    Primeiro commit

--- src/main/java/com/cryptoadz/CryptoAdzApplication.java
+++ src/main/java/com/cryptoadz/CryptoAdzApplication.java
@@ -11,18 +11,18 @@ public class CryptoAdzApplication {
 
 	
     public static void main(String[] args) {
-     
-
+    
         SpringApplication.run(CryptoAdzApplication.class, args);
     }
 }
 
 
-/**  // Carrega o arquivo .env na raiz do projeto (padrão)
+/**   // Carrega o arquivo .env na raiz do projeto (padrão)
 Dotenv dotenv = Dotenv.load();
 
 // Setando as variáveis do .env como propriedades do sistema para o Spring ler
 dotenv.entries().forEach(entry -> {
     System.setProperty(entry.getKey(), entry.getValue());
 });
+
  */
